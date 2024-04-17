a91516827a611e22bfe3367009a14e84e254cded
Anne Shen <anneshen251@gmail.com>
2024-04-17 03:26:48 -0400

    persist server

--- Users/routes.js
+++ Users/routes.js
@@ -1,5 +1,5 @@
 import * as dao from './dao.js';
-let globalCurrentUser;
+let currentUser = null;
 export default function UserRoutes(app) {
   const createUser = async (req, res) => {
     try {
@@ -49,9 +49,8 @@ export default function UserRoutes(app) {
       if (user) {
         res.status(400).json({ message: 'Username already taken' });
       }
-      const currentUser = await dao.createUser(req.body);
+      currentUser = await dao.createUser(req.body);
       req.session['currentUser'] = currentUser;
-      globalCurrentUser = currentUser;
       res.json(currentUser);
     } catch (error) {
       console.error(error);
@@ -62,10 +61,9 @@ export default function UserRoutes(app) {
   const signin = async (req, res) => {
     try {
       const { username, password } = req.body;
-      const currentUser = await dao.findUserByCredentials(username, password);
+      currentUser = await dao.findUserByCredentials(username, password);
       if (currentUser) {
         req.session['currentUser'] = currentUser;
-        globalCurrentUser = currentUser;
         res.json(currentUser);
       } else {
         res.status(401).json({ message: 'Invalid username or password' });
@@ -81,7 +79,7 @@ export default function UserRoutes(app) {
     res.sendStatus(200);
   };
   const profile = (req, res) => {
-    const currentUser = req.session['currentUser'];
+    currentUser = req.session['currentUser'];
     if (!currentUser) {
       res.sendStatus(401);
       return;
